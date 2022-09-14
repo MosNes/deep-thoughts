c2be91d98ac012ae9d1d52c4ca75d93b675f0575
MosNes <7hekarl@gmail.com>
2022-09-14 19:44:29 -0400

    added optional search by username function

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,8 +2,12 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
 	Query: {
-		thoughts: async () => {
-			return Thought.find().sort({ createdAt: -1 });
+        //parent has to be passed as first param, so 2nd param can be accessed
+		thoughts: async (parent, {username}) => {
+            //if username exists, add it as property of params, else set params to empty object
+            const params = username ? { username } : {};
+            //find thought by params, and sort by createdAt time, descending (-1)
+			return Thought.find(params).sort({ createdAt: -1 });
 		},
 	},
 };
