69ae3c9177f6fdd080b790accc20892f04a5f5f9
MosNes <7hekarl@gmail.com>
2022-09-18 10:11:23 -0400

    added Link components to ThoughtList component

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ThoughtList = ({ thoughts, title }) => {
 	if (!thoughts.length) {
@@ -8,24 +9,32 @@ const ThoughtList = ({ thoughts, title }) => {
 	return (
 		<div>
 			<h3>{title}</h3>
-            {/* if thoughts are present */}
+			{/* if thoughts are present */}
 			{thoughts &&
-                // map through each thought and render it as an element on the page
+				// map through each thought and render it as an element on the page
 				thoughts.map((thought) => (
-                    //create key for react to reference
+					//create key for react to reference
 					<div key={thought._id} className="card mb-3">
 						<p className="card-header">
-							{thought.username+' '}
+							<Link
+								to={`/profile/${thought.username}`}
+								style={{ fontWeight: 700 }}
+								className="text-light"
+							>
+								{thought.username}
+							</Link>{' '}
 							thought on {thought.createdAt}
 						</p>
 						<div className="card-body">
-							<p>{thought.thoughtText}</p>
-							<p className="mb-0">
-								Reactions: {thought.reactionCount} || Click to{' '}
-                                {/* if thought has reactions, use 'see', else use 'start' */}
-								{thought.reactionCount ? 'see' : 'start'} the
-								discussion!
-							</p>
+							<Link to={`/thought/${thought._id}`}>
+								<p>{thought.thoughtText}</p>
+								<p className="mb-0">
+									Reactions: {thought.reactionCount} || Click to{' '}
+									{/* if thought has reactions, use 'see', else use 'start' */}
+									{thought.reactionCount ? 'see' : 'start'} the
+									discussion!
+								</p>
+							</Link>
 						</div>
 					</div>
 				))}
