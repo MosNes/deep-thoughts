0d888b4c2db47b6719e2d3bf6677f45ad250220e
MosNes <7hekarl@gmail.com>
2022-09-18 17:43:40 -0400

    added reaction list component

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,43 +1,46 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHT } from '../utils/queries'
+import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
-const SingleThought = props => {
+const SingleThought = (props) => {
+	//get thoughtId from URL params
+	const { id: thoughtId } = useParams();
+	console.log(thoughtId);
 
-  //get thoughtId from URL params
-  const {id: thoughtId} = useParams();
-  console.log(thoughtId)
-  
-  //use thoughtId to make a GraphQL query to the DB
-  const { loading, data } = useQuery(QUERY_THOUGHT, {
-    variables: { id: thoughtId }
-  });
+	//use thoughtId to make a GraphQL query to the DB
+	const { loading, data } = useQuery(QUERY_THOUGHT, {
+		variables: { id: thoughtId },
+	});
 
-  //if the returned data contains a 'thought' property, return it. Otherwise return an empty object.
-  const thought = data?.thought || {};
-  console.log(thought);
+	//if the returned data contains a 'thought' property, return it. Otherwise return an empty object.
+	const thought = data?.thought || {};
+	console.log(thought);
 
-  //if data returned from DB yet, render a 'loading' page
-  if (loading) {
-    return <div>Loading...</div>
-  }
+	//if data returned from DB yet, render a 'loading' page
+	if (loading) {
+		return <div>Loading...</div>;
+	}
 
-  return (
-    <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            {thought.username}
-          </span>{' '}
-          thought on {thought.createdAt}
-        </p>
-        <div className="card-body">
-          <p>{thought.thoughtText}</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="card mb-3">
+				<p className="card-header">
+					<span style={{ fontWeight: 700 }} className="text-light">
+						{thought.username}
+					</span>{' '}
+					thought on {thought.createdAt}
+				</p>
+				<div className="card-body">
+					<p>{thought.thoughtText}</p>
+				</div>
+			</div>
+			{thought.reactionCount > 0 && (
+				<ReactionList reactions={thought.reactions} />
+			)}
+		</div>
+	);
 };
 
 export default SingleThought;
